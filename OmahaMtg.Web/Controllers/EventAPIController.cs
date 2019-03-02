71af8a3dd1b26cb1db4367a5d257664f0f573b8a
Brian Olson <beolson@hero4hire.com>
2019-03-01 21:08:27 -0600

    Add event controller, and re-add banner add

---
+++ OmahaMtg.Web/Controllers/EventAPIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OmahaMtg.Events;
+
+namespace OmahaMtg.Web.Controllers
+{
+    [Route("api/Event")]
+    public class EventAPIController : ApiController
+    {
+        private OmahaMtg.Events.IEventManager _em;
+        public EventAPIController()
+        {
+            _em = new EventManager();
+        }
+
+        // GET: api/EventAPI
+        public IEnumerable<EventInfo> Get(bool past = false)
+        {
+            if (past)
+            {
+                return _em.GetLatestEventsWithVideos(5);
+            }
+            else
+            {
+                return _em.GetUpcomingEvents(5);
+            }
+
+        }
+
+
+    }
+}
